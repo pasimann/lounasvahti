f9987862847d573955f54fd00bd51fb99d3aed97
tahartik <taneli.hartikainen@digia.com>
2017-11-24 14:59:14 +0200

    Make it rain.

--- index.js
+++ index.js
@@ -2,30 +2,27 @@ require('dotenv/config')
 require('isomorphic-fetch')
 
 const moment = require('moment')
+const express = require('express')
 const { CronJob } = require('cron')
 const { WebClient } = require('@slack/client')
 
-/**
- * Handle to the Slack WebClient.
- */
+const EDIBLES_REGEX = [
+  /pi(ts|zz)a/i,
+  /itse\s?tehty/i,
+  /wien\w*\s*(leike|leikkeet)?/i,
+  /kiev\w*\s*kana/i,
+  /porsaan(leike|leikkeit)/i
+]
 const slack = new WebClient(process.env.SLACK_API_TOKEN)
 
-/**
- * The job to run on each pattern tick.
- */
 let job = null
 try {
   job = new CronJob(process.env.CRON_PATTERN, () => {
-    getMenu(new Date())
-      .then((menu) => {
-        return new Promise((resolve, reject) => {
-          slack.chat.postMessage('bot-playground', 'testi', { as_user: 'pekka' }, (err) => {
-            if (err) {
-              return reject(err)
-            }
-            return resolve()
-          })
-        })
+    return getMenu(new Date())
+      .then(filterEdibles)
+      .then(postEdiblesToSlack)
+      .then((message) => {
+        console.log(`Posted the following message:\n${message}`)
       })
       .catch(console.error)
   })
@@ -34,11 +31,17 @@ try {
   process.exit(1)
 }
 
-job.start()
+express()
+  .get('/', (req, res) => res.json({ foo: 'bar' }))
+  .listen(process.env.PORT, (err) => {
+    if (err) {
+      console.error(err)
+      return process.exit(1)
+    }
+    job.start()
+    console.log(`Listening...`)
+  })
 
-/**
- * Get the menu.
- */
 function getMenu (date) {
   if (moment(date).isValid) {
     const y = moment(date).get('year')
@@ -48,4 +51,19 @@ function getMenu (date) {
       .then(response => response.json())
   }
   return Promise.reject(new Error(`Invalid date: ${date}`))
+}
+
+function filterEdibles (menu) {
+  return Promise.resolve(
+    menu.courses.filter(course => !!EDIBLES_REGEX.find(re => re.test(course.title_fi))))
+}
+
+function postEdiblesToSlack (edibles) {
+  return new Promise((resolve, reject) => {
+    const courses = edibles.map(edible => edible.title_fi).join('\n')
+    const message = `Alakerrassa seuraavaa:\n\n${courses}`
+    slack.chat.postMessage('lounascuckold', message, { as_user: 'pekka' }, (err) => {
+      return err ? reject(err) : resolve(message)
+    })
+  })
 }
